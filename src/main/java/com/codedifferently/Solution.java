c05dd088d00e5fcc0da890ca50c76bde88c154c7
Yas Wood <yaswood@Yass-MacBook-Air.local>
2022-07-26 09:29:27 -0400

    done

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,28 @@
 package com.codedifferently;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Solution {
     public String howManyLettersDoYouSee(String input){
-        return null;
+        String[] chars = input.split("");
+        StringBuilder str = new StringBuilder();
+        Map<String,Integer> map = new TreeMap<String,Integer>();
+        for (String string:chars) {
+            map.merge(string,1, Integer::sum);
+        }
+
+        for(String string: map.keySet()){
+            str.append(string);
+            str.append(":");
+            str.append(map.get(string));
+            str.append(" ");
+        }
+
+        return str.toString().substring(0,str.length()-1);
+
+
+
     }
 }
